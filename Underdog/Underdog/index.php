b783e732a32032bc52723053dd1553bda11e6a0b
Michael-K-M <57261773+Michael-K-M@users.noreply.github.com>
2020-10-13 13:50:47 +0200

    Test 2
    Test 2

--- Underdog/Underdog/index.php
+++ Underdog/Underdog/index.php
@@ -45,12 +45,12 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0">MrD Here to stay</h1>
+            <h1 class="m-0">Testing 2</h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Dashboard v1</li>
+              <li class="breadcrumb-item active">Testing 2</li>
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
